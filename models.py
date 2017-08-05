e386da98b08def34ed635ef1604fdfba5c6e57ff
Edward Tang <tkwedward@gmail.com>
2017-08-04 21:23:04 -0700

    yes

--- models.py
+++ models.py
@@ -4,11 +4,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.files.storage import FileSystemStorage
-from redactor.fields import RedactorField
 
 class Jobs(models.Model):
     job = models.CharField(max_length=50)
-    body = RedactorField(default="")
+    body = models.TextField(default="")
     image = models.ImageField(null=True, blank=True)
     tag = models.CharField(max_length=20, default="工運史")
 
